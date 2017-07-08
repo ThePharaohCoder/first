94110a2a870ba1f4d65d03e41403335b43f889d1
Arthanios Ghaly <arsany.ghaly2014@gmail.com>
2017-07-08 00:45:32 -0500

    this is the first commit

---
+++ ReadFile.h
@@ -0,0 +1,25 @@
+#if !defined READ_FILE
+#define READ_FILE
+
+#include "Text.h"
+
+#include <fstream>
+using namespace std;
+
+class ReadFile
+{
+   private:
+   ifstream input_file;
+   bool _eof;
+   bool closed;
+
+
+   public:
+   ReadFile(const char* file_name);
+   ~ReadFile();
+   String* readLine();
+   bool eof();
+   void close();
+
+};
+#endif
